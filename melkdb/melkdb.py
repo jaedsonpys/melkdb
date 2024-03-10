d259aa2db706cafcb643eee78facbc17bca4c8e9
Jaedson Silva <jaedson.dev@proton.me>
2024-03-10 13:56:06 -0300

    feat: create database if not exist in "MelkDB()"

--- melkdb/melkdb.py
+++ melkdb/melkdb.py
@@ -6,3 +6,11 @@ MELKDB_STORAGE_PATH = os.path.join(HOME_PATH, '.melkdb.databases')
 
 if not os.path.isdir(MELKDB_STORAGE_PATH):
     os.mkdir(MELKDB_STORAGE_PATH)
+
+
+class MelkDB:
+    def __init__(self, name: str):
+        self._db_path = os.path.join(MELKDB_STORAGE_PATH, name)
+        
+        if not os.path.isdir(self._db_path):
+            os.mkdir(self._db_path)
