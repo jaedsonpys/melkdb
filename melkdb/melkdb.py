2c297ebde007bd951cc12c2e091b8c997c8e6f51
Jaedson Silva <jaedson.dev@proton.me>
2024-03-10 14:12:31 -0300

    feat: create method to add items to database

--- melkdb/melkdb.py
+++ melkdb/melkdb.py
@@ -17,10 +17,34 @@ class MelkDB:
         if not os.path.isdir(self._db_path):
             os.mkdir(self._db_path)
 
-        print(self._create_item('Jaedson'))
-
     @staticmethod
     def _create_item(value: str) -> bytes:
         vlen = len(value)
         item = struct.pack(f'h {vlen}s', vlen, value.encode())
         return item
+
+    def add(self, key: str, value: str) -> None:
+        klen = str(len(key))
+        first_letter = key[0]
+        last_letter = key[-1]
+
+        first_box_path = os.path.join(self._db_path, klen)
+
+        if not os.path.isdir(first_box_path):
+            os.mkdir(first_box_path)
+
+        second_box_path = os.path.join(first_box_path, first_letter)
+
+        if not os.path.isdir(second_box_path):
+            os.mkdir(second_box_path)
+
+        third_box_path = os.path.join(second_box_path, last_letter)
+
+        if not os.path.isdir(third_box_path):
+            os.mkdir(third_box_path)
+
+        data_path = os.path.join(third_box_path, 'data.melkdb')
+        item = self._create_item(value)
+
+        with open(data_path, 'wb') as f:
+            f.write(item)
