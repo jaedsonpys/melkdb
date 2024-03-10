3a23484d180fe11eb073edae791cd62778660bff
Jaedson Silva <jaedson.dev@proton.me>
2024-03-10 13:54:49 -0300

    feat: create MELKDB_STORAGE_PATH if not exists

--- melkdb/melkdb.py
+++ melkdb/melkdb.py
@@ -3,3 +3,6 @@ from pathlib import Path
 
 HOME_PATH = Path().home()
 MELKDB_STORAGE_PATH = os.path.join(HOME_PATH, '.melkdb.databases')
+
+if not os.path.isdir(MELKDB_STORAGE_PATH):
+    os.mkdir(MELKDB_STORAGE_PATH)
