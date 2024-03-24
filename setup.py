e5084df0cbc8ecf79f8122b91d6d89185ceb2916
Jaedson Silva <jaedson.dev@proton.me>
2024-03-24 11:59:27 -0300

    build: create package setup

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from melkdb.__version__ import __version__
+
+with open(f'README.md', 'r') as reader:
+    readme = reader.read()
+
+setup(
+    author='Jaedson Silva',
+    author_email='jaedson.dev@proton.me',
+    name='melkdb',
+    description='MelkDB: A faster key-value database',
+    version=__version__,
+    long_description_content_type='text/markdown',
+    long_description=readme,
+    license='MIT License',
+    install_requires=['pycryptodome==3.20'],
+    packages=['melkdb'],
+    url='https://github.com/jaedsonpys/melkdb',
+    project_urls={
+        'License': 'https://github.com/jaedsonpys/melkdb/blob/master/LICENSE'
+    },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Database :: Database Engines/Servers'
+    ],
+    keywords=['database', 'noSQL', 'key-value', 'db', 'engine']
+)
