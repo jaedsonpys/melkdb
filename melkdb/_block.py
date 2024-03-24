4285e1f915674f7b0e9393dd2401f374c662a8d1
Jaedson Silva <jaedson.dev@proton.me>
2024-03-24 12:21:56 -0300

    docs: create docstring to Block class

--- melkdb/_block.py
+++ melkdb/_block.py
@@ -3,15 +3,48 @@ import os
 
 class Block:
     def __init__(self, database_path: str) -> None:
+        """Create a instance of Block class
+
+        In MelkDB, a block is a path of directories that
+        are organized in sequence according to the
+        specified key. The first part of the block is
+        created using the length of the key, the second
+        part is created using the first letter of the key,
+        and the last part is created using the last
+        letter of the key.
+
+        With this, we have an optimized path to facilitate
+        the search for items in the database.
+
+        :param database_path: Database path
+        :type database_path: str
+        """
+
         self._db_path = database_path
 
     def get_path(self, key: str) -> str:
+        """Mount key block.
+
+        :param key: Item key
+        :type key: str
+        :return: Block path
+        :rtype: str
+        """
+
         klen = str(len(key))
         first_letter = key[0]
         last_letter = key[-1]
         return os.path.join(self._db_path, klen, first_letter, last_letter)
 
     def make_path(self, key: str) -> str:
+        """Create a block.
+
+        :param key: Item path
+        :type key: str
+        :return: Block path
+        :rtype: str
+        """
+
         klen = str(len(key))
         first_letter = key[0]
         last_letter = key[-1]
