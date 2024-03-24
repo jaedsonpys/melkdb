e3f3af0d9816d2aaf87e56b3e43c0aa59ca29997
Jaedson Silva <jaedson.dev@proton.me>
2024-03-24 12:29:03 -0300

    docs: create docstring to Item class

--- melkdb/_item.py
+++ melkdb/_item.py
@@ -12,9 +12,30 @@ BOOL_TYPE = -3
 
 class Item:
     def __init__(self, crypto: Union[Cryptography, None] = None) -> None:
+        """Create a instance of Item class.
+
+        :param crypto: Cryptography class instance, defaults to None
+        :type crypto: Union[Cryptography, None], optional
+        """
+
         self._crypto = crypto
 
     def encode(self, value: Union[str, int, float, bool]) -> bytes:
+        """Encode item value.
+
+        The format of pure data encoding is: two bytes
+        to store the size of the value and a fixed or
+        dynamic size of bytes to store the value.
+
+        Value will be encrypted if cryptography is enabled.
+
+        :param value: Item value
+        :type value: Union[str, int, float, bool]
+        :raises ValueNotSupportedError: If value is not supported
+        :return: Encoded value
+        :rtype: bytes
+        """
+
         if isinstance(value, str):
             vlen = len(value)
             pack_fmt = f'{vlen}s'
@@ -39,6 +60,16 @@ class Item:
         return item
 
     def decode(self, buf_reader: BufferedReader) -> Union[str, int, float, bool]:
+        """Decode a item value from file.
+
+        Value will be decrypted if cryptography is enabled.
+
+        :param buf_reader: File buffered reader
+        :type buf_reader: BufferedReader
+        :return: Decoded value
+        :rtype: Union[str, int, float, bool]
+        """
+
         if self._crypto:
             buf_reader = self._crypto.decrypt(buf_reader.read())
             buf_reader = BytesIO(buf_reader)
