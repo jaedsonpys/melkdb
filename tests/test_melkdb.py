9462f0be93811bea234b6a61816f726e04a412cf
Jaedson Silva <jaedson.dev@proton.me>
2024-03-24 21:10:04 -0300

    test: create test to encode items without cryptography

--- tests/test_melkdb.py
+++ tests/test_melkdb.py
@@ -1,7 +1,15 @@
+import struct
+from io import BytesIO
+
 import bupytest
 
 from melkdb import crypto
+from melkdb import _item
 from melkdb import exceptions
+
+INT_TYPE = -1
+FLOAT_TYPE = -2
+BOOL_TYPE = -3
 
 
 class TestCryptography(bupytest.UnitTest):
@@ -32,6 +40,26 @@ class TestCryptography(bupytest.UnitTest):
             self.assert_true(True)
         else:
             self.assert_true(False, message='Expected exception not raised')
+
+
+class TestItem(bupytest.UnitTest):
+    def __init__(self):
+        super().__init__()
+
+        self.item_handle = _item.Item()
+
+    def test_encode_without_cryptography(self):
+        data = 'MelkDB'
+        data_len = len(data)
+
+        encoded = self.item_handle.encode(data)
+        item_io = BytesIO(encoded)
+
+        enc_data_len, = struct.unpack('<h', item_io.read(2))
+        enc_data, = struct.unpack(f'<{enc_data_len}s', item_io.read(enc_data_len))
+
+        self.assert_expected(enc_data_len, data_len, message='Invalid data length')
+        self.assert_expected(enc_data.decode(), data, message='Invalid encoded data')
 
 
 if __name__ == '__main__':
